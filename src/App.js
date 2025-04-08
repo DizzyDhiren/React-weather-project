6079b984271b7cb7f2105bec537cc69412305783
DizzyDhiren <dhiren007patel@gmail.com>
2025-04-09 00:58:50 +0100

    added js pages : navbar and home and linked to the root App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 
 import './App.css';
+import Navbar from './Navbar';
+import Home from './Home';
 
 function App() {
   // This is the main component of the application
   // It serves as the entry point for the React app
   // and contains the main layout and structure
   // of the application.
-  const title = "Welcome to my React Weather App";
+  
 
   return (
     <div className="App">
+      <Navbar />
       <div className="content">
-        <h1>{title}</h1>
+        <Home />
       </div>
     </div>
   );
---
+++ src/Navbar.js
@@ -0,0 +1,17 @@
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <h1>Weather app</h1>
+            <div className="links">
+                <a href="/">Home</a>
+                <a href="/create">Home2</a>
+                <a href="/create">Home3</a>
+
+            </div>
+        </nav>
+
+    );
+}
+ 
+export default Navbar
+    
