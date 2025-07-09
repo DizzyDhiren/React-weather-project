4d53be84506fe936bad726b1111bf1eb1fb73961
DizzyDhiren <dhiren007patel@gmail.com>
2025-07-10 00:43:32 +0100

    Added api key and url for the search function

--- src/api.js
+++ src/api.js
@@ -2,8 +2,19 @@
 const API_KEY = '0ca8b02eb16b7b3e0d9eae4b8dfd4792';
 const BASE_URL = 'https://api.openweathermap.org/data/2.5';
 
-export const fetchWeatherByCity = async (city) => {
-  const response = await fetch(`${BASE_URL}/weather?q=${city}&appid=${API_KEY}&units=metric`);
-  if (!response.ok) throw new Error('Failed to fetch weather');
-  return await response.json();
+
+
+const search = async (city) => {
+  try {
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
+
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data);
+
+  } catch (error) {
+    console.error("Error fetching weather data:", error);
+  }
 };
+
+export default search;
